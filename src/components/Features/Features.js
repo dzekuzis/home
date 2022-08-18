4185da210313d1c724dab484296b395bc47753ae
dzekuzis <linas.jasauskas@gmail.com>
2022-08-18 13:39:00 +0300

    made nav bar with routes

---
+++ src/components/Features/Features.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Features.css';
+
+const Features = () => (
+  <div className="Features" >
+    Features Component
+  </div>
+);
+
+
+
+export default Features;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Home.css';
+
+const Home = () => (
+  <div className="Home" data-testid="Home">
+    Home Component
+  </div>
+);
+
+
+export default Home;
